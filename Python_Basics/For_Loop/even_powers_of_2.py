e69c9bcdaa5575f625007750ad09676186ffb31e
Vyary <vyaryw@gmail.com>
2022-05-31 02:03:36 +0300

    Upload/Update

---
+++ Python_Basics/For_Loop/even_powers_of_2.py
@@ -0,0 +1,5 @@
+end_number = int(input())
+
+for power in range(0, end_number + 1, 2):
+    result = 2 ** power
+    print(result)
---
+++ Python_Basics/For_Loop/sum_numbers.py
@@ -0,0 +1,7 @@
+number_of_numbers = int(input())
+result = 0
+
+for number in range(number_of_numbers):
+    result += int(input())
+
+print(result)
